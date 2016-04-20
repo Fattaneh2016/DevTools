2650ce83b1801224e7a3928eefb9f6474a4f0b5b
Fattaneh2016 <fatameri@hotmail.com>
2016-04-19 21:22:12 -0400

    Console for debuging javascript

---
+++ DevTools/DevTools/DevTools.js
@@ -0,0 +1,26 @@
+﻿var stepnumber = 1;
+var thing='ants';
+function DoStuff() {
+    console.time('steps time');
+    console.log('Hi');
+
+    console.warn('Ohhhh');
+    console.error('be careful ');
+    console.debug('Debuging');
+    console.info('please note');
+
+
+
+    console.group('steps');
+
+    FirstStep();
+    stepnumber++;
+
+    function FirstStep() {
+        console.group( 'step %i ',stepnumber);
+        console.log('Finding %s', thing);
+        console.group('%c my group', "font-size:x-large");
+        console.timeEnd('steps time');
+    }
+    
+}
